bbc0198c78def27759e759f1bee678b91a2e8f09
abdalafo98 <abdalafo.abubaker@gmail.com>
2021-08-23 09:38:21 +0300

    backend flutter app

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 require("./db/db");
 const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
 
 app.use(express.json());
 
@@ -10,7 +12,7 @@ const articleModel = require("./routes/routers/article");
 app.use(cors());
 app.use(articleModel);
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
 });
